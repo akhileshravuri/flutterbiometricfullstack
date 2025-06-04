4a75c6d671a140f8ce1e83156da0263bfe12d032
akhilesh ravuri <akhileshravuri@gmail.com>
2025-06-04 22:11:38 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:biometric_auth/biometric_auth.dart';
+import 'package:authenticationpackage/biometric_auth.dart';
 
 void main() {
   runApp(const MyApp());
